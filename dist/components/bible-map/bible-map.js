455b3afd7361433eb78e68663c13201339a22206
Adam Bertrand <ajbertra91@gmail.com>
2021-10-24 15:05:09 -0400

    cleans up files

--- dist/components/bible-map/bible-map.js
+++ dist/components/bible-map/bible-map.js
@@ -4,24 +4,24 @@ import { define, element } from "base-web-components-core";
 import { lit } from "base-web-components-lit-html";
 import { connect } from "base-web-components-store";
 import "../base-map";
-import { scrollStore, scrollStoreJourneySelector } from "../../providers/scroll";
+import { scrollStore, scrollStoreJourneySelector, } from "../../providers/scroll";
 function AJBBibleMapTemplate(host) {
-    console.log('host.position: ', host.position);
+    console.log("host.position: ", host.position);
     const showHideCss = {
-        'p0-is-visible': host.position === 0,
-        'p1-is-visible': host.position > 0 && host.position < 44.0,
-        'p2-is-visible': host.position > 0 && host.position <= 17.85,
-        'p3-is-visible': host.position > 0 && host.position <= 17.85,
-        'p4-is-visible': host.position > 0 && host.position <= 17.85,
-        'p5-is-visible': host.position > 0 && host.position <= 17.85,
-        'p6-is-visible': host.position > 17.85 && host.position <= 30.46,
-        'p7-is-visible': host.position > 30.46 && host.position <= 37.5,
-        'p8-is-visible': host.position > 37.5 && host.position <= 44.0,
-        'p9-is-visible': host.position > 0 && host.position >= 44.0,
-        'p10-is-visible': host.position > 44.0 && host.position <= 45.8,
-        'p11-is-visible': host.position > 45.8 && host.position <= 50,
-        'p12-is-visible': host.position > 50 && host.position <= 64.12,
-        'p13-is-visible': host.position > 64.12 && host.position <= 100,
+        "p0-is-visible": host.position === 0,
+        "p1-is-visible": host.position > 0 && host.position < 44.0,
+        "p2-is-visible": host.position > 0 && host.position <= 17.85,
+        "p3-is-visible": host.position > 0 && host.position <= 17.85,
+        "p4-is-visible": host.position > 0 && host.position <= 17.85,
+        "p5-is-visible": host.position > 0 && host.position <= 17.85,
+        "p6-is-visible": host.position > 17.85 && host.position <= 30.46,
+        "p7-is-visible": host.position > 30.46 && host.position <= 37.5,
+        "p8-is-visible": host.position > 37.5 && host.position <= 44.0,
+        "p9-is-visible": host.position > 0 && host.position >= 44.0,
+        "p10-is-visible": host.position > 44.0 && host.position <= 45.8,
+        "p11-is-visible": host.position > 45.8 && host.position <= 50,
+        "p12-is-visible": host.position > 50 && host.position <= 64.12,
+        "p13-is-visible": host.position > 64.12 && host.position <= 100,
     };
     return html `
     <section class="ajb-bible-map__map">
@@ -35,7 +35,9 @@ function AJBBibleMapTemplate(host) {
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}"><strong>Acts 13</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}">
+          <strong>Acts 13</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p2 ${classMap(showHideCss)}">
           Now there were in the church that was at
           <strong>Antioch</strong>...Barnabas...and Saul.[1]
@@ -54,61 +56,75 @@ function AJBBibleMapTemplate(host) {
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}"><strong>Acts 13</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}">
+          <strong>Acts 13</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p6 ${classMap(showHideCss)}">
           Now when Paul and his company loosed from Paphos, they came to
           <strong>Perga</strong> in Pamphylia...[13]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}"><strong>Acts 13</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}">
+          <strong>Acts 13</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p7 ${classMap(showHideCss)}">
           But when they departed from Perga, they came to
-          <strong>Antioch</strong> in Pisidia, and went into the synagogue on the
-          sabbath day...[14]
+          <strong>Antioch</strong> in Pisidia, and went into the synagogue on
+          the sabbath day...[14]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}"><strong>Acts 13</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}">
+          <strong>Acts 13</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p8 ${classMap(showHideCss)}">
           But they shook off the dust of their feet against them, and came unto
           <strong>Iconium</strong>.[51]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}"><strong>Acts 14</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}">
+          <strong>Acts 14</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p10 ${classMap(showHideCss)}">
-          And when there was an assault made both of the Gentiles, and also of the
-          Jews with their rulers, to use them despitefully, and to stone them,
-          They were ware of it, and fled unto <strong>Lystra</strong> and
+          And when there was an assault made both of the Gentiles, and also of
+          the Jews with their rulers, to use them despitefully, and to stone
+          them, They were ware of it, and fled unto <strong>Lystra</strong> and
           <strong>Derbe</strong>, cities of Lycaonia, and unto the region that
           lieth round about: And there they preached the gospel...[5-7]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}"><strong>Acts 14</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}">
+          <strong>Acts 14</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p11 ${classMap(showHideCss)}">
           And there came thither certain Jews from Antioch and Iconium, who
-          persuaded the people, and, having stoned Paul, drew him out of the city,
-          supposing he had been dead. Howbeit, as the disciples stood round about
-          him, he rose up, and came into the city: and the next day he departed
-          with Barnabas to <strong>Derbe</strong>.[19,20]
+          persuaded the people, and, having stoned Paul, drew him out of the
+          city, supposing he had been dead. Howbeit, as the disciples stood
+          round about him, he rose up, and came into the city: and the next day
+          he departed with Barnabas to <strong>Derbe</strong>.[19,20]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}"><strong>Acts 14</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}">
+          <strong>Acts 14</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p12 ${classMap(showHideCss)}">
-          And when they had preached the gospel to that city, and had taught many,
-          they returned again to <strong>Lystra</strong>, and to
+          And when they had preached the gospel to that city, and had taught
+          many, they returned again to <strong>Lystra</strong>, and to
           <strong>Iconium</strong>, and <strong>Antioch</strong>...[21]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}"><strong>Acts 14</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}">
+          <strong>Acts 14</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p13 ${classMap(showHideCss)}">
           And after they had passed throughout Pisidia, they came to Pamphylia.
-          And when they had preached the word in <strong>Perga</strong>, they went
-          down into <strong>Attalia</strong>: And thence sailed to
+          And when they had preached the word in <strong>Perga</strong>, they
+          went down into <strong>Attalia</strong>: And thence sailed to
           <strong>Antioch</strong>...[24-26]
         </p>
       </div>
@@ -118,7 +134,7 @@ function AJBBibleMapTemplate(host) {
 const ajbBibleMapElement = {
     journey: connect(scrollStore, scrollStoreJourneySelector),
     position: ({ journey }) => {
-        let result = 0;
+        const result = 0;
         if (journey?.j1a?.position > 0 && journey?.j1a?.position < 100) {
             return journey?.j1a?.position / 2;
         }
--- src/components/bible-map/bible-map.ts
+++ src/components/bible-map/bible-map.ts
@@ -5,7 +5,10 @@ import { lit } from "base-web-components-lit-html";
 import { connect, status, StoreStatus } from "base-web-components-store";
 import "../base-map";
 import { Journey } from "../../providers/scroll/scroll-store-model";
-import { scrollStore, scrollStoreJourneySelector } from "../../providers/scroll";
+import {
+  scrollStore,
+  scrollStoreJourneySelector,
+} from "../../providers/scroll";
 
 interface AJBBibleMap extends HTMLElement {
   journey: Journey;
@@ -14,23 +17,23 @@ interface AJBBibleMap extends HTMLElement {
 }
 
 function AJBBibleMapTemplate(host: AJBBibleMap): TemplateResult {
-  console.log('host.position: ', host.position);
+  console.log("host.position: ", host.position);
   const showHideCss = {
-    'p0-is-visible': host.position === 0,
-    'p1-is-visible': host.position > 0 && host.position < 44.0, // Acts 13 title
-    'p2-is-visible': host.position > 0 && host.position <= 17.85,
-    'p3-is-visible': host.position > 0 && host.position <= 17.85,
-    'p4-is-visible': host.position > 0 && host.position <= 17.85,
-    'p5-is-visible': host.position > 0 && host.position <= 17.85,
-    'p6-is-visible': host.position > 17.85 && host.position <= 30.46,
-    'p7-is-visible': host.position > 30.46 && host.position <= 37.5,
-    'p8-is-visible': host.position > 37.5 && host.position <= 44.0,
-    'p9-is-visible': host.position > 0 && host.position >= 44.0, // Acts 14 title
-    'p10-is-visible': host.position > 44.0 && host.position <= 45.8,
-    'p11-is-visible': host.position > 45.8 && host.position <= 50,
-    'p12-is-visible': host.position > 50 && host.position <= 64.12,
-    'p13-is-visible': host.position > 64.12 && host.position <= 100,
-  }
+    "p0-is-visible": host.position === 0,
+    "p1-is-visible": host.position > 0 && host.position < 44.0, // Acts 13 title
+    "p2-is-visible": host.position > 0 && host.position <= 17.85,
+    "p3-is-visible": host.position > 0 && host.position <= 17.85,
+    "p4-is-visible": host.position > 0 && host.position <= 17.85,
+    "p5-is-visible": host.position > 0 && host.position <= 17.85,
+    "p6-is-visible": host.position > 17.85 && host.position <= 30.46,
+    "p7-is-visible": host.position > 30.46 && host.position <= 37.5,
+    "p8-is-visible": host.position > 37.5 && host.position <= 44.0,
+    "p9-is-visible": host.position > 0 && host.position >= 44.0, // Acts 14 title
+    "p10-is-visible": host.position > 44.0 && host.position <= 45.8,
+    "p11-is-visible": host.position > 45.8 && host.position <= 50,
+    "p12-is-visible": host.position > 50 && host.position <= 64.12,
+    "p13-is-visible": host.position > 64.12 && host.position <= 100,
+  };
   return html`
     <section class="ajb-bible-map__map">
       <h1 class="ajb-bible-map__title">Bible Maps</h1>
@@ -43,7 +46,9 @@ function AJBBibleMapTemplate(host: AJBBibleMap): TemplateResult {
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}"><strong>Acts 13</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}">
+          <strong>Acts 13</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p2 ${classMap(showHideCss)}">
           Now there were in the church that was at
           <strong>Antioch</strong>...Barnabas...and Saul.[1]
@@ -62,61 +67,75 @@ function AJBBibleMapTemplate(host: AJBBibleMap): TemplateResult {
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}"><strong>Acts 13</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}">
+          <strong>Acts 13</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p6 ${classMap(showHideCss)}">
           Now when Paul and his company loosed from Paphos, they came to
           <strong>Perga</strong> in Pamphylia...[13]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}"><strong>Acts 13</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}">
+          <strong>Acts 13</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p7 ${classMap(showHideCss)}">
           But when they departed from Perga, they came to
-          <strong>Antioch</strong> in Pisidia, and went into the synagogue on the
-          sabbath day...[14]
+          <strong>Antioch</strong> in Pisidia, and went into the synagogue on
+          the sabbath day...[14]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}"><strong>Acts 13</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p1 ${classMap(showHideCss)}">
+          <strong>Acts 13</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p8 ${classMap(showHideCss)}">
           But they shook off the dust of their feet against them, and came unto
           <strong>Iconium</strong>.[51]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}"><strong>Acts 14</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}">
+          <strong>Acts 14</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p10 ${classMap(showHideCss)}">
-          And when there was an assault made both of the Gentiles, and also of the
-          Jews with their rulers, to use them despitefully, and to stone them,
-          They were ware of it, and fled unto <strong>Lystra</strong> and
+          And when there was an assault made both of the Gentiles, and also of
+          the Jews with their rulers, to use them despitefully, and to stone
+          them, They were ware of it, and fled unto <strong>Lystra</strong> and
           <strong>Derbe</strong>, cities of Lycaonia, and unto the region that
           lieth round about: And there they preached the gospel...[5-7]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}"><strong>Acts 14</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}">
+          <strong>Acts 14</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p11 ${classMap(showHideCss)}">
           And there came thither certain Jews from Antioch and Iconium, who
-          persuaded the people, and, having stoned Paul, drew him out of the city,
-          supposing he had been dead. Howbeit, as the disciples stood round about
-          him, he rose up, and came into the city: and the next day he departed
-          with Barnabas to <strong>Derbe</strong>.[19,20]
+          persuaded the people, and, having stoned Paul, drew him out of the
+          city, supposing he had been dead. Howbeit, as the disciples stood
+          round about him, he rose up, and came into the city: and the next day
+          he departed with Barnabas to <strong>Derbe</strong>.[19,20]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}"><strong>Acts 14</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}">
+          <strong>Acts 14</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p12 ${classMap(showHideCss)}">
-          And when they had preached the gospel to that city, and had taught many,
-          they returned again to <strong>Lystra</strong>, and to
+          And when they had preached the gospel to that city, and had taught
+          many, they returned again to <strong>Lystra</strong>, and to
           <strong>Iconium</strong>, and <strong>Antioch</strong>...[21]
         </p>
       </div>
       <div class="ajb-bible-map__text-target position-absolute">
-        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}"><strong>Acts 14</strong> [KJV]</p>
+        <p class="ajb-text mb-0 p9 ${classMap(showHideCss)}">
+          <strong>Acts 14</strong> [KJV]
+        </p>
         <p class="ajb-text mt-0 mb-0 p13 ${classMap(showHideCss)}">
           And after they had passed throughout Pisidia, they came to Pamphylia.
-          And when they had preached the word in <strong>Perga</strong>, they went
-          down into <strong>Attalia</strong>: And thence sailed to
+          And when they had preached the word in <strong>Perga</strong>, they
+          went down into <strong>Attalia</strong>: And thence sailed to
           <strong>Antioch</strong>...[24-26]
         </p>
       </div>
@@ -127,10 +146,9 @@ function AJBBibleMapTemplate(host: AJBBibleMap): TemplateResult {
 const ajbBibleMapElement: CustomElement<AJBBibleMap> = {
   journey: connect(scrollStore, scrollStoreJourneySelector),
   position: ({ journey }: AJBBibleMap): number => {
-    let result = 0;
+    const result = 0;
     if (journey?.j1a?.position > 0 && journey?.j1a?.position < 100) {
       return journey?.j1a?.position / 2;
-
     }
     if (journey?.j1b?.position > 0 && journey?.j1b?.position <= 100) {
       return journey?.j1b?.position / 2 + 50;
